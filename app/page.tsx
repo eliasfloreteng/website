4f4ac653e6c9e805fb508fed3bc39824bdcb3891
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2024-01-22 18:04:01 +0100

    Change page description

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import RootNavigation from "./RootNavigation"
 export const metadata = {
   title: "Elias Floreteng",
   description:
-    "I am an enthusiastic software developer with a passion for creating high-quality, maintainable, and readable code. I am currently pursuing a Master's Degree in Computer Science at KTH Royal Institute of Technology in Stockholm, I bring with me over 6 years of prior programming experience. I have a genuine enthusiasm for learning, which allows me to quickly tackle new challenges.",
+    "I am an enthusiastic software developer with a passion for creating high-quality, maintainable, and readable code. I am currently pursuing a Master's Degree in Computer Science at KTH Royal Institute of Technology in Stockholm, I bring with me over 6 years of prior programming experience and I am always eager to learn new new things.",
 }
 
 export default function HomePage() {
