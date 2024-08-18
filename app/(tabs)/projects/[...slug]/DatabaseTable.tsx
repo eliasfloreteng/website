cad7a7a8f24e1d8926f8d65555939d85d29d030b
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2024-08-18 05:22:34 +0200

    Fix return type to be single jsx element

--- app/(tabs)/projects/[...slug]/DatabaseTable.tsx
+++ app/(tabs)/projects/[...slug]/DatabaseTable.tsx
@@ -108,90 +108,87 @@ export default async function DatabaseTable({
 }) {
   const blocks = await fetchBlockChildren(pageId)
 
-  return blocks.map(async (block) => {
-    if (block.type !== "child_database") {
-      return null
+  const block = blocks.find((block) => block.type === "child_database")
+  if (!block) return <></>
+
+  const pages = await fetchPages(block.id)
+  let items = (pages.results as PageObjectResponse[]).map((page) => ({
+    cover_url: page.cover,
+    props: page.properties,
+    url: page.url,
+    slug: urlToSlug(page.url),
+  }))
+
+  items.sort((a, b) => {
+    const nameA = a.props.Name
+    const nameB = b.props.Name
+    if (nameA.type !== "title" || nameB.type !== "title") return 0
+    if (nameA.title[0].plain_text < nameB.title[0].plain_text) {
+      return -1
     }
-    const pages = await fetchPages(block.id)
-    let items = (pages.results as PageObjectResponse[]).map((page) => ({
-      cover_url: page.cover,
-      props: page.properties,
-      url: page.url,
-      slug: urlToSlug(page.url),
-    }))
-    items.sort((a, b) => {
-      const nameA = a.props.Name
-      const nameB = b.props.Name
-      if (nameA.type !== "title" || nameB.type !== "title") return 0
-      if (nameA.title[0].plain_text < nameB.title[0].plain_text) {
-        return -1
-      }
-      if (nameA.title[0].plain_text > nameB.title[0].plain_text) {
-        return 1
-      }
-      return 0
-    })
-    items.sort((a, b) => {
-      const createdA = a.props.Created
-      const createdB = b.props.Created
-      if (
-        createdA.type !== "date" ||
-        createdB.type !== "date" ||
-        !createdA.date ||
-        !createdB.date
-      )
-        return 0
-      if (createdA.date.start < createdB.date.start) {
-        return 1
-      }
-      if (createdA.date.start > createdB.date.start) {
-        return -1
-      }
-      return 0
-    })
-
-    const columns = Object.keys(items[0].props).filter((key) => key !== "Name")
-
-    return (
-      <div
-        key={block.child_database.title}
-        className="prose mt-6 max-w-full overflow-x-auto"
-      >
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Cover</th>
-              <th>Title</th>
-              {columns.map((key) => {
-                return <th key={key}>{key}</th>
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => {
-              const title =
-                item.props.Name.type === "title"
-                  ? item.props.Name.title[0].plain_text
-                  : null
-              const cover_url = item.cover_url
-                ? item.cover_url.type === "file"
-                  ? item.cover_url.file.url
-                  : item.cover_url.external.url
-                : null
-              return (
-                <TableRow
-                  key={index}
-                  title={title || ""}
-                  cover_url={cover_url || GRADIENTS[index]}
-                  properties={item.props}
-                  columns={columns}
-                  highlight={slugs.includes(item.slug)}
-                />
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-    )
+    if (nameA.title[0].plain_text > nameB.title[0].plain_text) {
+      return 1
+    }
+    return 0
   })
+
+  items.sort((a, b) => {
+    const createdA = a.props.Created
+    const createdB = b.props.Created
+    if (
+      createdA.type !== "date" ||
+      createdB.type !== "date" ||
+      !createdA.date ||
+      !createdB.date
+    )
+      return 0
+    if (createdA.date.start < createdB.date.start) {
+      return 1
+    }
+    if (createdA.date.start > createdB.date.start) {
+      return -1
+    }
+    return 0
+  })
+
+  const columns = Object.keys(items[0].props).filter((key) => key !== "Name")
+
+  return (
+    <div className="prose mt-6 max-w-full overflow-x-auto">
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>Cover</th>
+            <th>Title</th>
+            {columns.map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => {
+            const title =
+              item.props.Name.type === "title"
+                ? item.props.Name.title[0].plain_text
+                : null
+            const cover_url = item.cover_url
+              ? item.cover_url.type === "file"
+                ? item.cover_url.file.url
+                : item.cover_url.external.url
+              : null
+            return (
+              <TableRow
+                key={index}
+                title={title || ""}
+                cover_url={cover_url || GRADIENTS[index]}
+                properties={item.props}
+                columns={columns}
+                highlight={slugs.includes(item.slug)}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
