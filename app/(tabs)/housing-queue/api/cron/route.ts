54b06cc68faa2eab954105de0e7215ecd85ec54a
Elias <18127101+elias123tre@users.noreply.github.com>
2024-09-02 14:47:45 +0200

    Add cron route for housing queue

---
+++ app/(tabs)/housing-queue/api/cron/route.ts
@@ -0,0 +1,48 @@
+import { fetchHousing } from "../../helpers"
+import { kv } from "@vercel/kv"
+import { type NextRequest, NextResponse } from "next/server"
+
+export const config = {
+  runtime: "edge",
+}
+
+export const dynamic = "force-dynamic" // static by default, unless reading the request
+
+export async function GET(_request: NextRequest) {
+  const housing = await fetchHousing({
+    maxRent: 7000,
+    maxRooms: 1,
+    noCorridors: true,
+    isStudent: true,
+  })
+
+  const firstHousing = housing.slice(0, 12)
+  const housingHash = firstHousing
+    .map((house) => {
+      return `${house.agencyType}-${house.agencyType === "agency" ? house.LägenhetId : house.objectNumber}`
+    })
+    .sort()
+    .join(",")
+  const oldHousing = await kv.get<string>("housing")
+  if (oldHousing === housingHash) {
+    return new NextResponse(
+      JSON.stringify({
+        message: "No new housing",
+      }),
+      {
+        status: 204,
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    )
+  }
+  console.log("oldHousing", oldHousing)
+  console.log("housingHash", housingHash)
+  await kv.set("housing", housingHash)
+  return new NextResponse(JSON.stringify(firstHousing), {
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+}
