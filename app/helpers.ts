f56a83766e38b1e071db170e42b4b79db70c13d0
Elias <18127101+elias123tre@users.noreply.github.com>
2024-09-14 01:49:22 +0200

    Update packages

--- app/helpers.ts
+++ app/helpers.ts
@@ -2,6 +2,7 @@ export async function safeParseJSON<T = unknown>(json: string) {
   try {
     return JSON.parse(json) as T
   } catch (e) {
+    console.log("Failed to parse JSON:")
     console.log(json)
     console.error(e)
     return null
