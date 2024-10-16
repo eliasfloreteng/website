cbfe3a83bd7f4ddd5bbf80967c00929625458242
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2024-10-16 02:08:41 +0200

    Update description text

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   ),
   title: "Elias Floreteng",
   description:
-    "I am an enthusiastic software developer with a passion for creating high-quality, maintainable, and readable code. I am currently pursuing a Master's Degree in Computer Science at KTH Royal Institute of Technology in Stockholm, I bring with me over 6 years of prior programming experience and I am always eager to learn new new things.",
+    "I have been programming for over 7 years and I am currently taking a Master's Degree in Computer Science at KTH Royal Institute of Technology in Stockholm. Parallel with this I co-founded and I am currently the CTO of the app Accord.",
   applicationName: "Elias Floreteng",
   appleWebApp: {
     title: "Elias Floreteng",
