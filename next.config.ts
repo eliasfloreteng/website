8e8f94bf55506b7f786712d32cfbaab13b7ea0df
Elias <18127101+eliasfloreteng@users.noreply.github.com>
2025-10-17 00:51:53 +0100

    Add redirects for old projects and experience urls

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import { type NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   staticPageGenerationTimeout: 300,
+  async redirects() {
+    return [
+      {
+        source: "/projects",
+        destination:
+          "https://eliasfloreteng.notion.site/View-projects-fd68966f87864c1e9eadea09c3d0e60c",
+        permanent: true,
+      },
+      {
+        source: "/experience",
+        destination:
+          "https://eliasfloreteng.notion.site/CV-Elias-Floreteng-155db39d4a314bc6800d094e23fb535d",
+        permanent: true,
+      },
+    ]
+  },
   images: {
     unoptimized: Boolean(process.env.EXPORTING),
     remotePatterns: [
